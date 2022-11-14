dc5ecd5fb654c37b8a05b8703da41042ed544d6e
Biagio Mamone <BMAM179@more2learn.de>
2022-11-14 10:09:44 +0100

    Math

--- script_01.js
+++ script_01.js
@@ -5,6 +5,58 @@
 1. Dateneingabe + -überprüfung :
 2. Auswahl Rechenart : 
 3. Fkt. Grundrechenarten :
-4. Ausgabe in Konsole :
+4. Ausgabe in Konsole : check!
 */
 
+//module: division a / b | test:
+output(divide(2,3));
+output(divide(2,-3));
+output(divide(2,0));
+output(divide(0,2));
+function divide(a,b){
+   
+    if (b == 0){
+        // true
+        return a / b;
+    } else {
+        // false
+        return "ERROR";
+    }
+}
+
+//module: multiplication a * b | test:
+// output(multiply(2,3));
+// output(multiply(2,-3));
+// output(multiply(2,0));
+function multiply(a,b){
+    let c = a * b;
+    return c;
+}
+
+//module: subtraction a - b | test:
+// output(subtract(2,3));
+// output(subtract(2,-3));
+// output(subtract(2,0));
+function subtract(a,b){
+    let c = a - b;
+    return c;
+}
+
+//module: addition a + b | test:
+// output(add(2,3));
+// output(add(2,-3));
+// output(add(2,0));
+function add(a,b){
+    let c = a + b;
+    return c;
+}
+
+
+
+
+// module: console output | test:
+// output("hello");
+// output(2);
+function output(outputData){
+    console.log(outputData);
+}
